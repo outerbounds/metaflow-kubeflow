cf17f2f604cc2fc1c9bad53b1ea00a4d09b41f63
Madhur Tandon <madhur@outerbounds.co>
2025-12-11 21:55:03 +0530

    make path discoverable

--- metaflow_extensions/kubeflow_pipelines_ext/plugins/mfextinit_kubeflow_pipelines_ext.py
+++ metaflow_extensions/kubeflow_pipelines_ext/plugins/mfextinit_kubeflow_pipelines_ext.py
@@ -1,3 +1,5 @@
+__mf_promote_submodules__ = ["kubeflow_pipelines"]
+
 CLIS_DESC = [("kubeflow-pipelines", ".kubeflow_pipelines.kubeflow_pipelines_cli.cli")]
 
 STEP_DECORATORS_DESC = [("kfp_internal", ".kubeflow_pipelines.kubeflow_pipelines_decorator.KFPInternalDecorator")]
