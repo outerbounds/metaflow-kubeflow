05ab86c4685b26f2f762c1d275b566e3c231fe50
Madhur Tandon <madhur@outerbounds.co>
2025-12-11 22:02:42 +0530

    fix path again

--- metaflow_extensions/kubeflow_pipelines_ext/plugins/mfextinit_kubeflow_pipelines_ext.py
+++ metaflow_extensions/kubeflow_pipelines_ext/plugins/mfextinit_kubeflow_pipelines_ext.py
@@ -1,5 +1,3 @@
-__mf_promote_submodules__ = ["kubeflow_pipelines"]
-
 CLIS_DESC = [("kubeflow-pipelines", ".kubeflow_pipelines.kubeflow_pipelines_cli.cli")]
 
 STEP_DECORATORS_DESC = [("kfp_internal", ".kubeflow_pipelines.kubeflow_pipelines_decorator.KFPInternalDecorator")]
